425db6c9d751a6220cded0369f21e9b7165e25e5
edson.horacavalcanti@gmail.com <edson.horacavalcanti@gmail.com>
2018-11-27 14:25:13 -0200

    Inclusão do QuerySelector + Separação do Código JS

---
+++ js/principal.js
@@ -0,0 +1,6 @@
+
+var titulo = document.querySelector(".titulo").textContent;
+ console.log(titulo);
+
+var paciente = document.querySelector(".paciente").querySelector(".info-nome").textContent;
+console.log(paciente);
