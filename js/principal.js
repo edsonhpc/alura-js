18c5a04531bf90dbf040b3b8cda0fe910f29d7ae
edson.horacavalcanti@gmail.com <edson.horacavalcanti@gmail.com>
2018-11-27 19:07:36 -0200

    Incluindo o Calculo do IMC no FrontEnd

--- js/principal.js
+++ js/principal.js
@@ -1,6 +1,21 @@
 
-var titulo = document.querySelector(".titulo").textContent;
- console.log(titulo);
+var titulo = document.querySelector(".titulo");
+    titulo.textContent = "Aparecida Nutricionista";
 
-var paciente = document.querySelector(".paciente").querySelector(".info-nome").textContent;
+
+
+var paciente = document.querySelector("#primeiro-paciente");
+var tdNome   = paciente.querySelector(".info-nome").textContent;
+var tdPeso   = paciente.querySelector(".info-peso").textContent;
+var tdAltura = paciente.querySelector(".info-altura").textContent;
+var tdImc    = paciente.querySelector(".info-imc");
+
+tdImc.textContent = (tdPeso / (tdAltura * tdAltura));
+
+console.log(tdNome + '  -> IMC = ' + (tdPeso / (tdAltura * tdAltura)));
+
+
+
 console.log(paciente);
+console.log(tdPeso);
+console.log(tdAltura);
