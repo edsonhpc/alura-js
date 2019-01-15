50d25dbbe5a7a98dcb928a9e2ddc866b523ea542
edson.horacavalcanti@gmail.com <edson.horacavalcanti@gmail.com>
2019-01-15 15:43:35 -0200

    Inclusão da Funcionalidade de Filtro Dinamico na Pesquisa Juntamentamente com o Uso da Expressão Regular na chamada do objeto test()

---
+++ js/filtra.js
@@ -0,0 +1,26 @@
+var campoFiltro = document.querySelector("#filtrar-tabela");
+
+    campoFiltro.addEventListener("input", function() {
+
+    var pacientes = document.querySelectorAll(".paciente");
+
+    if(this.value.length > 0){
+        for (var i = 0; i < pacientes.length; i++) {
+             var paciente = pacientes[i];
+             var tdNome = paciente.querySelector(".info-nome");
+             var nome = tdNome.textContent;
+             var expressao = new RegExp(this.value, "i"); //Chamada do objeto RegExp passando o valor a ser testado, + qual o tipo casesentitive o sentive
+             if ( !expressao.test(nome)){ // Chamo o método test informando o que eu quero testar
+               paciente.classList.add("invisivel");
+             }else{
+                paciente.classList.remove("invisivel");
+             }
+
+         }
+      }else{
+            for( var i = 0; i < pacientes.length; i++) {
+               var paciente = pacientes[i];
+                 paciente.classList.remove("invisivel");
+           }
+    }
+ });
